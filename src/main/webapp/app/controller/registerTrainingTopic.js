dea6564f08b142de456429e305ced1beaf8535dd
ouahib ADLI <ouahib.adli@viseo.com>
2017-03-13 16:30:29 +0100

    [develop] update vee-validate to vue.js addTrainingTopic

--- src/main/webapp/app/controller/registerTrainingTopic.js
+++ src/main/webapp/app/controller/registerTrainingTopic.js
@@ -2,62 +2,102 @@
  * Created by BBA3616 on 24/02/2017.
  */
 Vue.use(VueResource);
-Vue.use(VeeValidate);
 Vue.use(VueRouter);
 
 var TrainingTopic = new Vue({
     el: '#TrainingTopic',
     data: {
-        training:{
-            trainingTitle:'',
-            numberHalfDays:'',
-            topicDescription:''
+        training: {
+            trainingTitle: '',
+            numberHalfDays: '',
+            topicDescription: ''
         },
-        trainingToRegister:{},
-
-        topic:{
-            name:''
+        trainingTitle: '',
+        numberHalfDays: '',
+        topicDescription:'',
+        trainingToRegister: {},
+        topic: {
+            name: ''
         },
-        topicToRegister:{},
+        nameTheme: '',
+        trainingTitleMsg: '',
+        nameThemeMsg: '',
+        topicToRegister: {},
         optionsTopic: [],
-        isNewTrainingTitle:true,
-        isNewTopic:true,
-        confirmFormation:false,
+        isNewTrainingTitle: true,
+        isNewTopic: true,
+        confirmFormation: false,
         confirmTopic: false,
-
-        msgtrainingTitle:false,
-        msgnumberHalfDays:false,
-        msgtopic:false,
-        msgname:false,
-
-
-
+        msgtrainingTitle: false,
+        msgnumberHalfDays: false,
+        msgtopic: false,
+        msgname: false,
+        isTrainingTitleValid: true,
+        isNameThemeValid:true,
     },
+    watch: {
+        trainingTitle: function (value) {
+            this.VerifTrainingTitle(value, 'trainingTitleMsg');
+        },
+
+
+        trainingTitle: function (value) {
+            this.VerifTrainingTitle(value, 'trainingTitleMsg');
+        },
+
+
+        nameTheme: function (value) {
+            this.VerifnameTheme(value, 'nameThemeMsg');
+        },
+    },
+
     methods: {
+        VerifTrainingTitle(trainingTitle, msg) {
+            if (/^[a-zA-Z0-9-.'_@:+#%]*$/.test(trainingTitle)) {
+                this[msg] = '';
+                this.isTrainingTitleValid = true;
+            } else {
+                this[msg] = "Veuillez entrer un nom de formation valide (-.'_@:+#% autorisés)";
+                this.isTrainingTitleValid = false;
+
+            }
+        },
         messageTrainingTitle(){
-            if(this.training.trainingTitle == ''){
+            this.isTrainingTitleValid = true;
+            if (this.trainingTitle == '') {
                 this.msgtrainingTitle = true;
             }
         },
         messageNumberHalfDays(){
-            if(this.training.numberHalfDays == ''){
+            if (this.numberHalfDays == '') {
                 this.msgnumberHalfDays = true;
             }
         },
         messageTopic(){
-            if(this.training.topicDescription == ''){
+            if (this.topicDescription == '') {
                 this.msgtopic = true;
             }
         },
+
+        VerifnameTheme(nameTheme, msg) {
+            if (/^[a-zA-Z0-9-.'_@:+#%]*$/.test(nameTheme)) {
+                this[msg] = '';
+                this.isNameThemeValid = true;
+            } else {
+                this[msg] = "Veuillez entrer un nom de topic valide (-.'_@:+#% autorisés)";
+                this.isNameThemeValid = false;
+            }
+        },
         messageName(){
-            if(this.topic.name == ''){
+            this.isNameThemeValid = true;
+            if (this.nameTheme == '') {
                 this.msgname = true;
             }
         },
         resetTrainingForm() {
-            this.training.trainingTitle = '';
-            this.training.numberHalfDays = '';
-            this.training.topicDescription = '';
+            this.trainingTitle = '';
+            this.numberHalfDays = '';
+            this.topicDescription = '';
             this.trainingToRegister = {};
         },
         resetFormTheme(){
@@ -76,19 +116,24 @@ var TrainingTopic = new Vue({
                         this.resetTopicForm();
                     },
                     function (response) {
-                        console.log("Error: ",response);
+                        console.log("Error: ", response);
                         if (response.data.message == "trainingTitle") {
                             this.isNewTrainingTitle = false;
-                        }else{
+                        } else {
                             console.error(response);
                         }
                     }
                 );
         },
         verifyTrainingForm() {
-            this.training.trainingTitle = this.training.trainingTitle.replace(/ +/g, "");
-            this.messageTrainingTitle(); this.messageNumberHalfDays(); this.messageTopic();
-            if(!this.msgtrainingTitle && !this.msgnumberHalfDays && !this.msgtopic) {
+            this.trainingTitle = this.trainingTitle.replace(/ +/g, "");
+            this.training.trainingTitle = this.trainingTitle;
+            this.training.numberHalfDays = this.numberHalfDays;
+            this.training.topicDescription = this.topicDescription;
+            this.messageTrainingTitle();
+            this.messageNumberHalfDays();
+            this.messageTopic();
+            if (!this.msgtrainingTitle && !this.msgnumberHalfDays && !this.msgtopic) {
                 this.trainingToRegister = JSON.parse(JSON.stringify(this.training));
                 this.saveTrainingAction();
             }
@@ -96,23 +141,23 @@ var TrainingTopic = new Vue({
 
         updateTopics(){
             this.$http.get("api/themes").then(
-                function(response){
+                function (response) {
                     this.optionsTopic = response.data;
 
                     this.resetTopicForm();
                 },
-                function(response){
-                    console.log("Error: ",response);
+                function (response) {
+                    console.log("Error: ", response);
                     console.error(response);
                 }
             );
         },
         resetTopicForm() {
-            this.topic.name = '';
+            this.nameTheme = '';
             this.topicToRegister = {};
         },
         saveTopicAction() {
-            this.topicToRegister.name = this.topic.name.replace(" ", "").toUpperCase();  //delete useless spaces between words
+            this.topicToRegister.name = this.nameTheme.replace(" ", "").toUpperCase();  //delete useless spaces between words
 
             //post the form to the server
             this.$http.post("api/themes", this.topicToRegister)
@@ -122,7 +167,7 @@ var TrainingTopic = new Vue({
                         this.updateTopics();
                     },
                     function (response) {
-                        console.log("Error: ",response);
+                        console.log("Error: ", response);
                         if (response.data.message == "name") {
                             this.isNewTopic = false;
                         } else {
@@ -132,17 +177,15 @@ var TrainingTopic = new Vue({
                 );
         },
         verifyTopicForm() {
-            this.topic.name = this.topic.name.replace(/ +/g, "");
+            this.nameTheme = this.nameTheme.replace(/ +/g, "");
+            this.topic.name = this.nameTheme;
             this.messageName();
-            if(!this.msgname) {
+            if (!this.msgname) {
                 this.isNewTopic = true;
                 this.topicToRegister = JSON.parse(JSON.stringify(this.topic));
                 this.saveTopicAction();
             }
         }
-
-
     }
 });
-
 window.onload = TrainingTopic.updateTopics();
