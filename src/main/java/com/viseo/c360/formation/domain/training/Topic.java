d87eb2c0732ceb38f9fb22f0cac22c3a856286ba
ouahib ADLI <ouahib.adli@viseo.com>
2017-03-16 15:19:20 +0100

    [develop] update Theme

--- src/main/java/com/viseo/c360/formation/domain/training/Topic.java
+++ src/main/java/com/viseo/c360/formation/domain/training/Topic.java
@@ -2,15 +2,17 @@ package com.viseo.c360.formation.domain.training;
 
 
 import com.viseo.c360.formation.domain.BaseEntity;
-
+import com.viseo.c360.formation.dto.training.TopicDescription;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.NotNull;
 
 @Entity
 public class Topic extends BaseEntity {
 
     @NotNull
+    @Pattern(regexp=TopicDescription.Regex.NAME+"*")
     @Column(unique=true, nullable=false)
     String name;
 
