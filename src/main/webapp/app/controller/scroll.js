7e33f1a523d02ed3f0432220b1712bb8e5183638
xiangzhem <xiangzhe.meng@viseo.com>
2017-03-17 14:42:47 +0100

    [develop] Correction scroll & vitesse

--- src/main/webapp/app/controller/scroll.js
+++ src/main/webapp/app/controller/scroll.js
@@ -8,9 +8,9 @@ $('#scroll-down').click(function() {
 
 $('#test').bind('mousewheel DOMMouseScroll', function(event){
     if(event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-        $('#test').animate({scrollTop: "-=100"}, 10);
+        $('#test').animate({scrollTop: "-=100"}, 80);
     }
     else{
-        $('#test').animate({scrollTop: "+=100"}, 10);
+        $('#test').animate({scrollTop: "+=100"}, 80);
     }
 });
