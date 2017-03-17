1f292a40bdfacf6f63d2941274330c9f11928bda
Benjamin BATISTA <benjamin.batista@viseo.com>
2017-03-17 13:53:48 +0100

    [develop] Ajout du scroll avec la souris

--- src/main/webapp/app/controller/scroll.js
+++ src/main/webapp/app/controller/scroll.js
@@ -5,3 +5,12 @@ $('#scroll-up').click(function() {
 $('#scroll-down').click(function() {
     $('#test').animate({scrollTop: "+=100"}, 500);
 })
+
+$('#test').bind('mousewheel DOMMouseScroll', function(event){
+    if(event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+        $('#test').animate({scrollTop: "-=100"}, 100);
+    }
+    else{
+        $('#test').animate({scrollTop: "+=100"}, 100);
+    }
+});
