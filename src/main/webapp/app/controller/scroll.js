321c923275460839369a9acd55f7d00fbaeace27
Benjamin BATISTA <benjamin.batista@viseo.com>
2017-03-17 14:32:23 +0100

    [develop] Changement des chevrons

--- src/main/webapp/app/controller/scroll.js
+++ src/main/webapp/app/controller/scroll.js
@@ -8,9 +8,9 @@ $('#scroll-down').click(function() {
 
 $('#test').bind('mousewheel DOMMouseScroll', function(event){
     if(event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-        $('#test').animate({scrollTop: "-=100"}, 100);
+        $('#test').animate({scrollTop: "-=100"}, 10);
     }
     else{
-        $('#test').animate({scrollTop: "+=100"}, 100);
+        $('#test').animate({scrollTop: "+=100"}, 10);
     }
 });
