7a33c61ec2735c7aef75f2d6530e241c22b63ee1
xiangzhem <xiangzhe.meng@viseo.com>
2017-03-27 12:05:53 +0200

    [US#GF15]Adaptation BDD pour la formulaire Connexion

---
+++ src/main/webapp/app/controller/authentification.js
@@ -0,0 +1,42 @@
+/**
+ * Created by XME3612 on 27/03/2017.
+ */
+
+
+
+
+new Vue({
+    el: '#authentification',
+    data:{
+        user:{
+            email:'',
+            password:''
+        },
+        email:'',
+        password:'',
+        userToRegister:{},
+        isErrorAuthentification:false
+    },
+    methods:{
+        ResetUserForm(){
+
+        },
+        VerifyForm(){
+            //à compléter
+            this.userToRegister = JSON.parse(JSON.stringify(this.user));
+            this.VerifyUserByDatabase();
+        },
+        VerifyUserByDatabase(){
+            this.$http.post("api/user", this.userToRegister)
+                .then(
+                    function (response) {
+                        this.ResetUserForm();
+                }).then(function () {
+                    window.location.pathname = '/pageblanche.html';
+                }).catch(function () {
+                    this.user.password = "";
+                    this.isErrorAuthentification = true;
+                });
+        }
+    }
+});
