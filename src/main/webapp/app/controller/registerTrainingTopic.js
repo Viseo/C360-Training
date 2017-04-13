5740f21698c12290631bb274ed73bd1d76be7fc5
xiangzhem <xiangzhe.meng@viseo.com>
2017-04-13 10:15:33 +0200

    [US#GF5] Correction la validation Version 2

--- src/main/webapp/app/controller/registerTrainingTopic.js
+++ src/main/webapp/app/controller/registerTrainingTopic.js
@@ -573,7 +573,7 @@ Vue.component('add-session-panel', {
         },
 
         isTrainingTitleInAddSessionEmpty(){
-            if (this.trainingTitleInAddSession == '' || this.trainingTitleInAddSession == undefined) {
+            if (this.state.trainingTitle == '' || this.state.trainingTitle == undefined) {
                 this.trainingTitleInAddSessionErrorMessage = true;
             }
         },
@@ -799,9 +799,6 @@ Vue.component('add-session-panel', {
                                                    style = "width:100%"/>                                                                        
                                         </div>
                                 </div>
-                                <div class = "row " style = "margin-bottom: 30px;">
-                                    
-                                </div>                                                       
                             </form>
                         </div>
                     </div>
