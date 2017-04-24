ae741bcb8272a217e17d91964e018ec78bc5a5a1
Nihel <nihelbengamra@gmail.com>
2017-04-24 10:30:36 +0200

    [US#GF6] Gestion des demandes de formations et recherche des collaborateurs et les faire  enregistrer.

--- src/main/webapp/app/controller/assignCollaborator.js
+++ src/main/webapp/app/controller/assignCollaborator.js
@@ -8,14 +8,20 @@ let assignCollaborator = Vue.component('assign-collaborator', {
     data: function () {
         return {
             allSessions:[],
-            sessionIdChosen:{},
+            sessionIdChosen:'',
             nomrequest:[],
-            allCollaboratorsIdChosen:[10,12]
+            allCollaboratorsIdChosen:[],
+            checkedNames: true,
+            validatedCollab: [],
+            allCollaborators: [],
+            allCollaboratorsName:[],
+            value: '',
+            collaboratorsFound: [],
+            displayCollaborators: false,
+            noCollaboratorsFound: false
         }
     },
-    methods : {
 
-    },
     template: `
         <div class="container-fluid">
             <div class="row">
@@ -27,17 +33,12 @@ let assignCollaborator = Vue.component('assign-collaborator', {
                     </div>
                     <div class="row">
                         <div id="assignCollaborator">
-                    
-                        <input-text
-                            width="20%"
-                            label="Sélectionner session"
-                            :value="sessionIdChosen"
-                            @input="updateV1"
-                            placeholder="Sélectionner session"
-                            :collection="allSessions"
-                            print-prop="id"
-                            type="select">
-                        </input-text>
+                                <select 
+                                        class="form-control" 
+                                        v-model="sessionIdChosen"
+                                        >
+                                        <option :value="session.id" v-for="session in allSessions" > {{session.trainingDescription.topicDescription.name}} - {{session.trainingDescription.trainingTitle}} -  {{session.beginning}} - {{session.location}}</option>
+                                </select>
                         <!--<select class="col-sm-10 col-md-10 col-lg-10">
                         </select>-->
                         </br>
@@ -46,22 +47,32 @@ let assignCollaborator = Vue.component('assign-collaborator', {
                                  <div class="row">
                                      <h4 class="col-sm-12 col-md-12 col-lg-12">Liste des collaborateurs</h4>
                                         <div class="checkbox col-sm-12 col-md-12 col-lg-12" >
-                                             <label><input type="checkbox" value="">Afficher les demandes</label>
+                                             <label><input type="checkbox" value="" v-model="checkedNames">Afficher les demandes</label>
+                                             
                                         </div> 
+                                        
                                            
                                  </div>
                                  <div class="panel panel-default">
-                                 <div class="panel-body">
-                                   <div class=" col-sm-12 col-md-12 col-lg-12 searchField">
-                                                <span class="glyphicon glyphicon-search" ></span>
-                                                    <typeahead class="col-sm-12 col-dm-12 col-lg-12" placeholder="Nom ou prénom du collaborateur">
-                                                    </typeahead> 
-                                                                                      
-                                        </div>
-                                 
-                                </div>
-                                </div>
-                                </div>
+                                    <div class="panel-body">
+                                         <div class=" col-sm-12 col-md-12 col-lg-12 searchField">
+                                                <span class="glyphicon glyphicon-search" @click="storeCollaboratorsFound" value=""></span>
+                                                <typeahead class="col-sm-12 col-dm-12 col-lg-12" v-model="value" v-bind:data="allCollaboratorsName" placeholder="Nom ou prénom du collaborateur"></typeahead> 
+                                                                               
+                                         </div><br/><br/>
+                                         
+                                         <div align="center" >
+                                           <div v-show="noCollaboratorsFound" style="margin-top:10px;"> Aucun résultat trouvé </div>
+                                             <table class="tabCentring" >
+                                                 <tr v-for="collaborator in nomrequest">
+                                                     <td @click="moveCollabRight(collaborator)" >{{collaborator.lastName}} {{collaborator.firstName}} </td>  
+                                                     <td @click="moveCollabRight(collaborator)"><span  class="glyphicon glyphicon-circle-arrow-right green" style="top:2px"></span></td>
+                                                 </tr>
+                                             </table>
+                                         </div>
+                                    </div>
+                                 </div>
+                            </div>
                                  
                                  
                                 <div class="col-sm-6 col-md-6 col-lg-6">
@@ -72,13 +83,22 @@ let assignCollaborator = Vue.component('assign-collaborator', {
                                      </div>
                                     </div>
                                      <div class="panel panel-default">
-                                
-                                 <div class="panel-body">
-                                 
-                                </div>
-                                </div>
+                                        <div class="panel-body">
+                                            <br/><br/>
+                                             <div align="center">
+                                                 <table class="tabCentring">
+                                                     <tr v-for="collaborator in validatedCollab">
+                                                      <td @click="moveCollabLeft(collaborator)"><span  class="glyphicon glyphicon-circle-arrow-left blue" style="top:2px"></span></td>
+                                                         <td @click="moveCollabLeft(collaborator)">{{collaborator.lastName}} {{collaborator.firstName}} </td>  
+                                                     </tr>
+                                                 </table>
+                                             </div>
+                                            
+                                        </div>
+                                        
+                                     </div>
                                  </div>
-                            <button class="col-sm-offset-4 col-dm-offset-4 col-lg-offset-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary">Enregistrer</button>
+                            <button class="col-sm-offset-4 col-dm-offset-4 col-lg-offset-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary" @click="saveCollabInSessions()">Enregistrer</button>
                         </div>
                     </div>
                 </div>
@@ -86,47 +106,122 @@ let assignCollaborator = Vue.component('assign-collaborator', {
             
 
 `,
+
     mounted: function () {
         this.GatherAllSessions();
+        this.gatherCollaboratorsFromDatabase();
     },
-    methods:{
+    methods: {
         updateV1 (v) {
             this.sessionIdChosen = v;
         },
         GatherAllSessions(){
             this.$http.get("api/sessions").then(
-                function (response) {
+                function(response) {
                     console.log("success to get all sessions");
                     this.allSessions = response.data;
                 },
-                function (response) {
+                function(response) {
                     console.log("Error: ", response);
                     console.error(response);
                 });
         },
         GatherAllRequestsBySession(){
-            this.$http.get("api/requests/session/"+ this.sessionIdChosen + "/collaborators").then(
-                function (response) {
+            this.$http.get("api/requests/session/" + this.sessionIdChosen + "/collaborators").then(
+                function(response) {
                     console.log("success to get all requests");
                     console.log(response.data);
                     this.nomrequest = response.data;
                 },
-                function (response) {
+                function(response) {
                     console.log("Error: ", response);
                     console.error(response);
                 });
         },
         AddCollaboratorsToTrainingSession(){
-                this.$http.put("api/sessions/" + this.sessionIdChosen + "/" + this.allCollaboratorsIdChosen + "/collaborators").then(
-                    function (response) {
-                        console.log("success to modify");
-                        console.log(response.data);
-                    },
-                    function (response) {
-                        console.log("Error: ", response);
-                        console.error(response);
-                    });
+            this.$http.put("api/sessions/" + this.sessionIdChosen + "/" + this.allCollaboratorsIdChosen + "/collaborators").then(
+                function(response) {
+                    console.log("success to modify");
+                    console.log(response.data);
+                },
+                function(response) {
+                    console.log("Error: ", response);
+                    console.error(response);
+                });
         },
-    }
+        verifyCheckedNames(checkedNames) {
+            if (this.checkedNames === true) {
+                this.GatherAllRequestsBySession();
+            }
+            else {
+                this.nomrequest = ""
+            }
+
+        },
+        moveCollabRight(nameCollab){
+            this.validatedCollab.push(nameCollab);
+            this.nomrequest.indexOf(nameCollab);
+            this.nomrequest.splice(this.nomrequest.indexOf(nameCollab),1);
+        },
+        moveCollabLeft(nameCollab){
+            this.nomrequest.push(nameCollab);
+            this.validatedCollab.indexOf(nameCollab);
+            this.validatedCollab.splice(this.validatedCollab.indexOf(nameCollab),1);
+        },
+        saveCollabInSessions(){
+           var i;
+            for (i = 0; i < this.validatedCollab.length; i++){
+                this.allCollaboratorsIdChosen.push(this.validatedCollab[i].id);
+            }
+            this.AddCollaboratorsToTrainingSession();
+            this.validatedCollab.splice(0,this.validatedCollab.length);
+            this.allCollaboratorsIdChosen.splice(0,this.allCollaboratorsIdChosen.length);
+        },
+        gatherCollaboratorsFromDatabase(){
+            this.$http.get("api/collaborateurs").then(
+                function (response) {
+                    this.allCollaborators = response.data;
+                    this.allCollaborators.sort(function (a, b) {
+                        return (a.lastName > b.lastName) ? 1 : ((b.lastName > a.lastName) ? -1 : 0);
+                    });
+                    this.selectCollaborators();
+                },
+                function (response) {
+                    console.log("Error: ", response);
+                    console.error(response);
+                }
+            );
+        },
+
+        storeCollaboratorsFound(){
+            this.collaboratorsFound.splice(0, this.collaboratorsFound.length);
+            this.displayCollaborators = true;
+            this.$http.get("api/collaborateurs").then(function(response){
+                for (index in this.allCollaborators)
+                {
+                     if (this.allCollaborators[index].lastName.indexOf(this.searchFormatted) != -1) {
+                     this.collaboratorsFound.push(this.allCollaborators[index]);
+                    }
+                }
+                this.noCollaboratorsFound = (this.collaboratorsFound.length == 0) ? true : false;
+               this.value = null;
+            });
+        },
+        selectCollaborators(){
+            for (index in this.allCollaborators) {
+                this.allCollaboratorsName.push(this.allCollaborators[index].lastName +"  " +this.allCollaborators[index].firstName);
+            }
+
+            //this.allCollaboratorsName = this.allCollaborators.firstName;
+        }
+    },
+        watch: {
+            sessionIdChosen: function(value) {
+                this.verifyCheckedNames(value);
+            },
+            checkedNames: function(value) {
+                this.verifyCheckedNames(value);
+            }
+        },
 });
 Vue.component('typeahead', VueStrap.typeahead);
