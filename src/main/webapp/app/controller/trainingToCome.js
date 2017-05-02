94591c4cc0de9257697e7cabf15c3a6fa10b8109
SJO3662 <soline.john@etudiant.univ-reims.fr>
2017-05-02 16:21:54 +0200

    [US#GF24] HTML pour formation à venir

---
+++ src/main/webapp/app/controller/trainingToCome.js
@@ -0,0 +1,25 @@
+/**
+ * Created by SJO3662 on 02/05/2017.
+ */
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+Vue.component('training-to-come', {
+    template: `
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 col-md-10 col-lg-12"></div>
+                    <div class="row">
+                        <div class="col-lg-9 col-md-9 text-center">
+                            <legend>Formation à venir</legend>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div id="training-to-come">
+                           
+                    </div>
+            </div>
+        </div>      
+
+`
+});
