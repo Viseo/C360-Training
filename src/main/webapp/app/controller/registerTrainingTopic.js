2ecfb0f8631b60e26ac5df05525d95dabfd63b84
HBI1 <hamza.bourakkadi@gmail.com>
2017-05-04 11:25:03 +0200

    [US#GF26] Modifs date picker (Weekend + Jours fériés)

--- src/main/webapp/app/controller/registerTrainingTopic.js
+++ src/main/webapp/app/controller/registerTrainingTopic.js
@@ -981,6 +981,34 @@ let AddSessionPanel = Vue.component('add-session-panel', {
                 return true;
             }
         },
+        JoursFeries (an){
+            var JourAn = new Date(an, "00", "01")
+            var FeteTravail = new Date(an, "04", "01")
+            var Victoire1945 = new Date(an, "04", "08")
+            var FeteNationale = new Date(an, "06", "14")
+            var Assomption = new Date(an, "07", "15")
+            var Toussaint = new Date(an, "10", "01")
+            var Armistice = new Date(an, "10", "11")
+            var Noel = new Date(an, "11", "25")
+            var SaintEtienne = new Date(an, "11", "26")
+
+            var G = an % 19
+            var C = Math.floor(an / 100)
+            var H = (C - Math.floor(C / 4) - Math.floor((8 * C + 13) / 25) + 19 * G + 15) % 30
+            var I = H - Math.floor(H / 28) * (1 - Math.floor(H / 28) * Math.floor(29 / (H + 1)) * Math.floor((21 - G) / 11))
+            var J = (an * 1 + Math.floor(an / 4) + I + 2 - C + Math.floor(C / 4)) % 7
+            var L = I - J
+            var MoisPaques = 3 + Math.floor((L + 40) / 44)
+            var JourPaques = L + 28 - 31 * Math.floor(MoisPaques / 4)
+            var Paques = new Date(an, MoisPaques - 1, JourPaques)
+            var VendrediSaint = new Date(an, MoisPaques - 1, JourPaques - 2)
+            var LundiPaques = new Date(an, MoisPaques - 1, JourPaques + 1)
+            var Ascension = new Date(an, MoisPaques - 1, JourPaques + 39)
+            var Pentecote = new Date(an, MoisPaques - 1, JourPaques + 49)
+            var LundiPentecote = new Date(an, MoisPaques - 1, JourPaques + 50)
+
+            return new Array(JourAn, LundiPaques, FeteTravail, Victoire1945, Ascension, LundiPentecote, FeteNationale, Assomption, Toussaint, Armistice, Noel, SaintEtienne)
+        },
         CalculateEndingDate(){
             if (/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/.test(this.beginningDate)) {
                 if(this.state.trainingChosen.numberHalfDays !=1)
@@ -995,8 +1023,11 @@ let AddSessionPanel = Vue.component('add-session-panel', {
                 var fin = dateObject;
                 fin.setDate(dayOfMonth+nbDays)
                 for (var d = debut; d <= fin; d.setDate(d.getDate() + 1)) {
-                    if(d.getDay() == 6 || d.getDay() == 0){
+                    console.log(this.JoursFeries(d.getFullYear()));
+                    console.log(d);
+                    if(d.getDay() == 6 || d.getDay() == 0 || this.JoursFeries(d.getFullYear()).filter(function(e) { return e.getTime() === d.getTime(); }).length > 0){
                         nbDays++;
+                        fin.setDate(fin.getDate() + 1);
                     }
                 }
                 dateObject.setDate(dayOfMonth + nbDays);
