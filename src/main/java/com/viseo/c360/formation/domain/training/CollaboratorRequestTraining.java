b4e37f80e4f264338ebacef02ee4700134f3ee44
Nihel <nihelbengamra@gmail.com>
2017-05-11 12:15:31 +0200

    [US#GF23] Récupération des trainings avec des status

---
+++ src/main/java/com/viseo/c360/formation/domain/training/CollaboratorRequestTraining.java
@@ -0,0 +1,7 @@
+package com.viseo.c360.formation.domain.training;
+
+/**
+ * Created by NBE3663 on 10/05/2017.
+ */
+public class CollaboratorRequestTraining {
+}
--- src/main/java/com/viseo/c360/formation/domain/training/RequestTrainingStatus.java
+++
@@ -1,8 +0,0 @@
-package com.viseo.c360.formation.domain.training;
-
-/**
- * Created by NBE3663 on 05/05/2017.
- */
-public enum RequestTrainingStatus {
-    PENDING,VALIDATE;
-}
