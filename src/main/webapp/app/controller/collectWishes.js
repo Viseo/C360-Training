36cbe68c3c6294bf04d07a144655c34b0b3ec870
Benjamin BATISTA <benjamin.batista@viseo.com>
2017-05-16 17:46:49 +0200

    [US#GF27] Ajout message confirmation

--- src/main/webapp/app/controller/collectWishes.js
+++ src/main/webapp/app/controller/collectWishes.js
@@ -7,15 +7,9 @@ let collectWishes = Vue.component('collect-wishes', {
     data: function () {
         return {
             showChevrons : true,
-            validateIcon : 'img/validate_icon_init.png',
-            refuseIcon : 'img/refuse_icon_init.png'
-
-            showChevrons : true,
             allWishes:[],
-            listWishesToUpdate:[
-                {"id":3,"version":1,"label":"BBB","collaborator":{"id":1,"version":0,"personnalIdNumber":"AAA1234","lastName":"ncjxkze","firstName":"cnjzk","email":"xiangzhe.meng@outlook.com","password":"123456","isAdmin":false},"vote_ok":[],"vote_ko":[],"checked":false},
-                {"id":4,"version":1,"label":"FFF","collaborator":{"id":1,"version":0,"personnalIdNumber":"AAA1234","lastName":"ncjxkze","firstName":"cnjzk","email":"xiangzhe.meng@outlook.com","password":"123456","isAdmin":false},"vote_ok":[],"vote_ko":[],"checked":true}
-                ]
+            listWishesToUpdate:[],
+            confirmUpdateWishes: false
         }
     },
 
@@ -42,76 +36,78 @@ let collectWishes = Vue.component('collect-wishes', {
                     <br/>
                         <img v-show="showChevrons" 
                              src="css/up.png" 
-                             id="scroll-up-2" 
+                             id="scroll-up-3" 
                              width="60" 
                              height="20" 
                              style="position: absolute; left:50%; z-index:1;">
                     </div>
                     </div>
-                    <div style="width: 100%; height: 360px; overflow-y:hidden; overflow-x:hidden;">
+                    <div id="scroll" style="width: 100%; height: 360px; overflow-y:hidden; overflow-x:hidden;">
                         <div class="row">
                             <ol style="list-style-type:none" class="rectangle-list">
-                                <li>
-                                    <a title="Vue.js" href="">
-                                    <img
-                                        @mouseover="validateIcon='img/validate_icon.png'"
-                                        @mouseleave="validateIcon='img/validate_icon_init.png'"
-                                        :src="validateIcon" 
+                                <li v-for="wish in allWishes">
+                                    <a :title="wish.label">
+                                    <img v-if="wish.checked==true"
+                                        :id="'validate'+wish.id"
+                                        @click="addWishToListWishes(wish, true)"
+                                       
+                                        src="img/validate_icon.png" 
                                         width="25" 
                                         height="25"
-                                        style="margin-left:20px;">
+                                        style="margin-left:20px; cursor:pointer;">
                                     <img
-                                        @mouseover="refuseIcon='img/refuse_icon.png'"
-                                        @mouseleave="refuseIcon='img/refuse_icon_init.png'"
-                                        :src="refuseIcon" 
+                                        v-else-if="wish.checked==null || wish.checked ==false"
+                                        :id="'validate'+wish.id"
+                                        @click="addWishToListWishes(wish, true)"
+                                        
+                                        src="img/validate_icon_init.png" 
                                         width="25" 
                                         height="25"
-                                        style="margin-left:50px;">
+                                        style="margin-left:20px; cursor:pointer;">
+                                    <img
+                                        v-if="wish.checked==false"
+                                        :id="'refuse'+wish.id"
+                                        @click="addWishToListWishes(wish, false)"
+                                        src="img/refuse_icon.png" 
+                                        width="25" 
+                                        height="25"
+                                        style="margin-left:50px; cursor:pointer">
+                                    <img
+                                        v-else-if="wish.checked == null || wish.checked ==true"
+                                        :id="'refuse'+wish.id"
+                                        @click="addWishToListWishes(wish, false)"
+                                        src="img/refuse_icon_init.png" 
+                                        width="25" 
+                                        height="25"
+                                        style="margin-left:50px; cursor:pointer">
                                     <img
                                         src="img/thumbs_up.png" 
                                         width="25" 
                                         height="25"
                                         style="margin-left:50px;">
-                                    <span style="color:#8c8c8c">13 votes</span>   
+                                    <span style="color:#8c8c8c">{{ wish.vote_ok.length}} votes</span>   
                                         <img
                                         src="img/thumbs_down.png" 
                                         width="25" 
                                         height="25"
                                         style="margin-left:20px;">
-                                       <span style="color:#8c8c8c">13 votes</span>
-                                     </a>
-                                </li> 
-<li>
-                                    <a title="Vue.js" href="">
-                                    <img
-                                        @mouseover="validateIcon='img/validate_icon.png'"
-                                        @mouseleave="validateIcon='img/validate_icon_init.png'"
-                                        :src="validateIcon" 
-                                        width="25" 
-                                        height="25"
-                                        style="margin-left:20px;">
-                                    <img
-                                        @mouseover="refuseIcon='img/refuse_icon.png'"
-                                        @mouseleave="refuseIcon='img/refuse_icon_init.png'"
-                                        :src="refuseIcon" 
-                                        width="25" 
-                                        height="25"
-                                        style="margin-left:50px;">  
-                                        <img
+                                       <span style="color:#8c8c8c">{{ wish.vote_ko.length }} votes</span> 
+                                        <img v-show="true"
                                         src="img/icon-novote.png" 
                                         width="25" 
                                         height="25"
                                         style="margin-left:155px;">
-                                       <span style="color:#8c8c8c">0 votes</span>
+                                       <span v-show="false" style="color:#8c8c8c">0 votes</span>
                                      </a>
-                                </li>                                   </ol>
+                                </li> 
+                                     </ol>
                         </div>
                     </div>
                     <div clas="row">
                     <div class="col-sm-12 col-md-3 col-lg-3 col-sm-offset-4 col-md-offset-4 col-lg-offset-4">
                         <img v-show="showChevrons" 
                              src="css/down.png" 
-                             id="scroll-down" 
+                             id="scroll-down-3" 
                              width="60" height="20" 
                              style="position: absolute; left:50%; top:95%; z-index:1;">
                     </div>
@@ -119,10 +115,12 @@ let collectWishes = Vue.component('collect-wishes', {
                     <div class="row">
                         <div class="col-sm-12 col-md-3 col-lg-3 col-sm-offset-5 col-md-offset-5 col-lg-offset-5">
                         <br/>
-                            <button class="btn btn-primary"  @click="">Enregistrer</button>
+                            <button class="btn btn-primary"  @click="updateWish()">Enregistrer</button>
                         </div>
                     </div>
                         <div style="width: 100%; height: 30px;">
+                        <br/>
+                                                <span v-show="confirmUpdateWishes" class="text-center color-green" style="margin-left:153px;margin-top:20px;" height="80px" width="250px">Vos modifications ont bien été enregistrées</span>
                         </div>
                 </div>
             </div>
@@ -131,9 +129,21 @@ let collectWishes = Vue.component('collect-wishes', {
 </div>`,
 
     mounted: function () {
+        Object.setPrototypeOf(this, BaseComponent(Object.getPrototypeOf(this)));
+        this.activateScrollUp('#scroll-up-3','#scroll');
+        this.activeScrollDown('#scroll-down-3','#scroll');
         this.getAllWishes();
     },
     methods: {
+
+        changeIconFromGreyToColor(idImage,colorIcon){
+                document.getElementById(idImage).src = colorIcon;
+        },
+
+        changeIconFromColorToGrey(idImage,greyIcon){
+                document.getElementById(idImage).src = greyIcon;
+        },
+
         getAllWishes(){
             this.$http.get("api/allwishes").then(
                 function (response) {
@@ -150,6 +160,14 @@ let collectWishes = Vue.component('collect-wishes', {
         },
         addWishToListWishes(wish,valueChecked){
             this.wishAlreadyInList = false;
+            if(valueChecked == true){
+                this.changeIconFromGreyToColor('validate'+wish.id,'img/validate_icon.png');
+                this.changeIconFromColorToGrey('refuse'+wish.id,'img/refuse_icon_init.png')
+            }
+            else if(valueChecked == false){
+                this.changeIconFromGreyToColor('refuse'+wish.id,'img/refuse_icon.png');
+                this.changeIconFromColorToGrey('validate'+wish.id,'img/validate_icon_init.png');
+            }
             for(var tmp in this.listWishesToUpdate){
                 if(this.listWishesToUpdate[tmp].id == wish.id){
                     this.listWishesToUpdate[tmp].checked = valueChecked;
@@ -164,12 +182,15 @@ let collectWishes = Vue.component('collect-wishes', {
             this.$http.post("api/ischeckedwishestoupdate",this.listWishesToUpdate).then(
                 function (response) {
                     console.log("success to update wishes");
+                    this.confirmUpdateWishes = true;
+                    setTimeout(function(){ this.confirmUpdateWishes = false; }.bind(this), 2000);
+                    this.getAllWishes();
                 },
                 function(response) {
                     console.log("Error: ", response);
                     console.error(response);
                 });
-        }
+        },
         returnAssignCollabPanel(){
             this.$router.push('/addTrainingTopic')
         }
