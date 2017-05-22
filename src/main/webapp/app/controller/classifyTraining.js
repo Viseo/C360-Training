5fe30edc71615e706a8774c095dae5041c119f43
SJO3662 <soline.john@etudiant.univ-reims.fr>
2017-05-22 14:32:49 +0200

    [US#GF11] Fichier javaScript pour la classification des formations.

---
+++ src/main/webapp/app/controller/classifyTraining.js
@@ -0,0 +1,62 @@
+/**
+ * Created by SJO3662 on 22/05/2017.
+ */
+
+let classifyTraining = Vue.component('classify-training', {
+    props: [],
+    data: function () {
+        return {
+            collaborator_id: '',
+            //feedback
+            //training dans feedback est sous la forme training au lieu de trianingdescription
+            feedback:{
+                "score":5,
+                "comment":"HELLO WORLD",
+                "training":{"id":3,"version":0,"trainingTitle":"FORMATION","numberHalfDays":3,"topic":{"id":2,"version":0,"name":"C"}}
+            },
+            allFeedbacks:[],
+            allTrainingScore:[]
+        }
+    },
+    mounted: function () {
+        Object.setPrototypeOf(this, BaseComponent(Object.getPrototypeOf(this)));
+    },
+    methods: {
+        addFeedback(feedback){
+            this.$http.post("api/feedback/"+this.collaborator_id,feedback).then(
+                function (response) {
+                    console.log("success to add a feedback");
+                },
+                function (response) {
+                    console.log("Error: ", response);
+                    console.error(response);
+                }
+            );
+        },
+        getAllFeedbacks(){
+            this.$http.get("api/feedbacks").then(
+                function (response) {
+                    console.log("success to get all feedbacks");
+                    this.allFeedbacks = response.data;
+                },
+                function (response) {
+                    console.log("Error: ", response);
+                    console.error(response);
+                }
+            );
+        },
+        getTrainingsScore(){
+            this.$http.get("api/trainingscore").then(
+                function (response) {
+                    console.log("success to get all training score");
+                    this.allTrainingScore = response.data;
+                },
+                function (response) {
+                    console.log("Error: ", response);
+                    console.error(response);
+                }
+            );
+        }
+    },
+});
+Vue.component('typeahead', VueStrap.typeahead);
---
+++ src/test/javaScript/spec/classifyTrainingSpec.js
@@ -0,0 +1,15 @@
+/**
+ * Created by SJO3662 on 22/05/2017.
+ */
+
+describe('classify training test', function () {
+    beforeEach(function () {
+
+        vmClassifyTraining = new classifyTraining().$mount();
+
+    });
+
+    afterEach(function () {
+
+    });
+});
